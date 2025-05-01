a6ed9d75717620b9659dc9c1af0e58724935a6e2
Omay <238maculo238@gmail.com>
2025-05-01 15:28:09 -0600

    fix for real this time

--- scripts/bettercsg.gd
+++ scripts/bettercsg.gd
@@ -2,7 +2,9 @@ extends StaticBody3D
 
 func _ready() -> void:
 	$CSGCombiner3D.visible = false
+	var n = 0
 	for i in $CSGCombiner3D.get_children():
+		n += 1
 		var col = CollisionShape3D.new()
 		var mes = MeshInstance3D.new()
 		if i is CSGBox3D:
@@ -19,7 +21,7 @@ func _ready() -> void:
 			mes.mesh.bottom_radius = i.radius
 			mes.mesh.height = i.height
 			mes.mesh.radial_segments = i.sides
-		mes.position = Vector3(round(i.position.x * 2.0) * 0.5, round(i.position.y * 2.0) * 0.5, round(i.position.z * 2.0) * 0.5)
+		mes.position = Vector3(round(i.position.x * 2.0) * 0.5, round(i.position.y * 2.0) * 0.5, round(i.position.z * 2.0) * 0.5) + Vector3(0.0001 * n, 0.0001 * n, 0.0001 * n)
 		mes.rotation = i.rotation
 		mes.scale = i.scale
 		mes.mesh.material = i.material
